b9fa429350b5d473f264a2522c12431d9f1437f1
Kate Bennert <kate.bennert@gmail.com>
2023-07-31 10:46:44 -0400

    Add notification dot

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -24,7 +24,7 @@ function NavBar() {
                 <ul className="nav-links">
                 <li><NavLink to={`/venues`} className="nav-link">Browse Venues</NavLink></li>
                 <li><NavLink to={`/my-events`} className="nav-link">My Events</NavLink></li>
-                <li><NavLink to={`/users/${user.id}`} className="nav-link">My Profile</NavLink></li>
+                <li><NavLink to={`/users/${user.id}`} className="nav-link">My Profile </NavLink><nobr className="notification-dot" >{user.name ? "" : "🔴"}</nobr></li>
                 <li><span onClick={handleLogoutClick} className="logout-span">Logout</span></li>
                 </ul>
             </nav>
@@ -39,7 +39,7 @@ function NavBar() {
                 <li><NavLink to={`/venues`} className="nav-link">Browse Venues</NavLink></li>
                 <li><NavLink to={`/my-events`} className="nav-link">My Events</NavLink></li>
                 <li><NavLink to={`/`} className="nav-link">My Clients</NavLink></li>
-                <li><NavLink to={`/`} className="nav-link">My Planner Profile</NavLink></li>
+                <li><NavLink to={`/`} className="nav-link">My Planner Profile </NavLink><nobr className="notification-dot" >{user.name ? "" : "🔴"}</nobr></li>
                 <li><NavLink to={`/`} className="nav-link">Create New Event</NavLink></li>
                 <li><span onClick={handleLogoutClick} className="logout-span">Logout</span></li>
                 </ul>
