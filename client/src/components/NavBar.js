62926551f01d3648f0e894bef377146b90c5c3b8
Kate Bennert <kate.bennert@gmail.com>
2023-08-03 19:18:51 -0400

    Fix NavBar bug

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -23,7 +23,7 @@ function NavBar() {
             </div>
                 <ul className="nav-links">
                 <li><NavLink to={`/venues`} className="nav-link">Browse Venues</NavLink></li>
-                <li><NavLink to={`/events`} className="nav-link">My Events</NavLink></li>
+                <li><NavLink to={`/events`} className="nav-link">My Events </NavLink></li>
                 <li><NavLink to={`/users/${user.id}`} className="nav-link">My Profile </NavLink><nobr className="notification-dot" >{user.name ? "" : "🔴"}</nobr></li>
                 <li><span onClick={handleLogoutClick} className="logout-span">Logout</span></li>
                 </ul>
