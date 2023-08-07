ad3078f6f91e0f1e434375717005dfae83b088db
Kate Bennert <kate.bennert@gmail.com>
2023-08-07 12:19:36 -0400

    Fix delete event dependent destory for comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     belongs_to :planner, class_name: 'Planner'
     belongs_to :client, class_name: 'Client'
     belongs_to :venue
-    has_many :comments
+    has_many :comments, dependent: :destroy
 
     validates :name, presence: true, length: { minimum: 2, maximum: 100 }
     validates :guest_num, :budget, numericality: { only_integer: true }
